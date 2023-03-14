4dc29407b6969a721152e83f91bafa140c951bf0
Matthew Clark <afromatt6288@hotmail.com>
2023-03-14 14:23:17 -0600

    Added solution to compare later

--- lib/anagram.py
+++ lib/anagram.py
@@ -20,3 +20,20 @@ class Anagram:
                 print("no")
         print(anagram_word)        
         return anagram_word
+
+
+##########################
+# Their Answer - much more concise than my own... even without all the prints i did...
+
+# class Anagram:
+#     def __init__(self, word):
+#         self.word_letters = sorted([letter for letter in word])
+
+#     def match(self, word_list):
+#         match_word_list = []
+
+#         for word in word_list:
+#             if sorted([letter for letter in word]) == self.word_letters:
+#                 match_word_list.append(word)
+
+#         return match_word_list
