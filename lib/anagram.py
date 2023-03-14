753559a3da34aa912160b440eb7b897fa47e5723
Matthew Clark <afromatt6288@hotmail.com>
2023-03-14 14:19:47 -0600

    Lab Completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,22 @@
 # your code goes here!
+
+class Anagram:
+
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list,):
+        sorted_words = []
+        for words in word_list:
+            sorted_words.append(sorted(words))
+        print(sorted_words)
+        print(sorted(self.word))
+        anagram_word = []            
+        for n in range(len(sorted_words)):
+            if sorted(self.word) == sorted_words[n]:
+                anagram_word.append(word_list[n])
+                print(f"yes, {anagram_word}")
+            else:
+                print("no")
+        print(anagram_word)        
+        return anagram_word
